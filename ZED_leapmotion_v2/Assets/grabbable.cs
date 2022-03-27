0a3740941bfbcb3a99d90c77975bf1d14235293b
Davide Zhang <11183058+davidezhang@users.noreply.github.com>
2022-03-26 22:45:42 -0400

    other

---
+++ ZED_leapmotion_v2/Assets/grabbable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabbable : MonoBehaviour
+{
+    public Transform hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collided");
+        transform.parent = collision.gameObject.transform.parent;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.parent = hand;
+        
+    }
+}
